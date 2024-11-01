07dd1a6f5440f03b7cdde73294bd5b8de6e6502c
harrishjoshi <dev.harishjoshi@gmail.com>
2024-11-01 12:39:52 +0545

    chore: Add todo-ui favicon

--- todo-ui/next.config.ts
+++ todo-ui/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   serverRuntimeConfig: {
     API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL
   },
--- todo-ui/pages/_app.tsx
+++ todo-ui/pages/_app.tsx
@@ -5,15 +5,16 @@ import Head from "next/head";
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-        <Head>
-            <title>Todo App</title>
-        </Head>
-        <Navbar />
-        <main>
-            <Component {...pageProps} />
-        </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <title>Todo App</title>
+            </Head>
+            <Navbar />
+            <main>
+                <Component {...pageProps} />
+            </main>
+        </>
+    );
 }
